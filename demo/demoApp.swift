67798e55dbfd84318c4d5d5374695fc5738f3669
Antoinette Marie Torres <act238@cornell.edu>
2025-02-28 09:12:43 -0500

    Initial Commit

---
+++ demo/demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  demoApp.swift
+//  demo
+//
+//  Created by Antoinette Marie Torres on 2/28/25.
+//
+
+import SwiftUI
+
+@main
+struct demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demoTests/demoTests.swift
@@ -0,0 +1,17 @@
+//
+//  demoTests.swift
+//  demoTests
+//
+//  Created by Antoinette Marie Torres on 2/28/25.
+//
+
+import Testing
+@testable import demo
+
+struct demoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
